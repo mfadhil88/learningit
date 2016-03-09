100aa71b19ce30449a4d1dd842e11cb61e6da3ef
mfadhil <mfadhil261@gmail.com>
2016-03-09 19:01:06 +0800

    add new pantun

--- 2nd_days.php
+++ 2nd_days.php
@@ -5,4 +5,11 @@ Ambil galah tolong jolokan<br/>
 Saya budak baru belajar<br/>
 Kalau salah tolong betulkan
 ';
+
+echo '
+Pulau pandan jauh ke tengah
+Gunung daik bercabang 3
+Hancur badan dikandung tanah
+Budi baik dikenang juga.
+';
 ?>
